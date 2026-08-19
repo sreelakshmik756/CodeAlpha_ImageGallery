f6a977ffff066eec37899bc71171ada21fd868b3
sreelakshmik756 <sreelakshmi.k756@gmail.com>
2026-08-19 21:57:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function filterImages(category) {
+    const images = document.querySelectorAll(".image");
+
+    images.forEach((image) => {
+        if (
+            category === "all" ||
+            image.classList.contains(category)
+        ) {
+            image.style.display = "block";
+        } else {
+            image.style.display = "none";
+        }
+    });
+}
